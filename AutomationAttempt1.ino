328a94eafcc7d251afd5f7587aeddc50c081662f
ProfGenki <threestarhouse@gmail.com>
2017-02-28 10:09:37 -0600

    Update AutomationAttempt1.ino

--- AutomationAttempt1.ino
+++ AutomationAttempt1.ino
@@ -1,38 +1,66 @@
+/*===============================================================================================================*
+*                                                                                                                *
+*                                              Variables                                                         *
+*                                                                                                                *
+*================================================================================================================*/
+
+//=======================================Libraries===========================================================\\
+
 #include <math.h> 
 #include <Servo.h>
+
+
+//=========================================Angles=============================================================\\
+
+float gyro;    
+float alpha;
+float gamma;
+
+float currentAngle; //Current angle of gyroscope. 
+
+//===========================================Buttons===========================================================\\
+
+#define buttonBlue = 7;
+#define buttonGreen = 6;
+#define buttonRed = 5;
+
+int prevButtonB;
+int prevButtonG;
+int prevButtonY;
+
+//=========================================Control============================================================\\
+
+int p;
+
+const int velox = 1;
+const int moveSpeed = 5;
+const int automateSpeed = "placeholder";
+
+const float sideA = "placeholder";
+const float sideB = "placeholder";
+
+float xList[] = {1};
+float yList[] = {1};
+
+//======================================Servo Objects========================================================\\
 
 Servo rotateServo;
 Servo baseServo;
 Servo armServo;
 Servo gripperServo;
 
-int p;
+int initPosR = "placeholder";
+int initPosB = "placeholder";
+int initPosA = "placeholder";
+int initPosG = "placeholder";
+int initPosO = "placeholder";
+int initPosS = "placeholder";
 
-float sideA;
-float sideB;
-
-float xList[] = {1};
-float yList[] = {1};
-
-float gyro;    
-float alpha;
-float gamma;
-
-int buttonBlue = 7;
-int buttonGreen = 6;
-int buttonRed = 5;
-
-int prevButtonB;
-int prevButtonG;
-int prevButtonY;
-
-int initPosR;
-int initPosB;
-int initPosA;
-int initPosG;
-int initPosO;
-int initPosS;
-
+/*===============================================================================================================*
+*                                                                                                                *
+*                                                Set Up                                                          *
+*                                                                                                                *
+*================================================================================================================*/
 
 void setup() {
  p = 0;
@@ -48,25 +76,50 @@ void setup() {
  gripperServo.attach(11);
  orientationServo.attach(12);
  sortServo.attach(13);
- 
- prevButtonB = digitalRead(buttonBlue);
- prevButtonG = digitalRead(buttonGreen);
- prevButtonR = digitalRead(buttonRed);
- 
- initPosR = rotateServo.read();
- initPosB = baseServo.read();
- initPosA = armServo.read();
- initPosG = gripperServo.read();
- 
+  
  initialize();
 }
 
+/*===============================================================================================================*
+*                                                                                                                *
+*                                               Main Loop                                                        *
+*                                                                                                                *
+*================================================================================================================*/
+
+
 void loop() {
-  // put your main code here, to run repeatedly:
  buttonCheck();
  servoMovement();
 }
 
+/*===============================================================================================================*
+*                                                                                                                *
+*                                               Functions                                                        *
+*                                                                                                                *
+*================================================================================================================*/
+                 
+//============================================Automation=========================================================\\                 
+
+//=====Automate=====\\ 
+void automate();
+   sortServo.write(placeholderVal);
+   delay(automateSpeed);
+   sortServo.write(oppositePlaceholderVal);
+   delay(automateSpeed);
+}
+     
+//=====Flip=====\\ 
+void flip(int o){
+ if(f == 0){
+   orientationServo.write(180);
+   orientation = 1;}
+ else if(f == 1){
+   orientationServo.write(0);
+   orientation = 0;}
+ status();
+}
+
+//=====Sort=====\\                  
 void sort(){
   flip();
   for(i = p; i < p + 10; i ++){
@@ -91,7 +144,10 @@ void sort(){
   p += 10;
   flip();
 }
-
+                 
+//=========================================Control Functions=======================================================\\
+                 
+//=====Button Check=====\\                                   
 void buttonCheck(){
  if(prevButtonB != digitalRead(buttonBlue)){
    sort();
@@ -103,27 +159,94 @@ void buttonCheck(){
    recall();
    prevButtonR = digitalRead(buttonRed);}
 }
-               
-void flip(int o){
- if(f == 0){
-   orientationServo.write(180);
-   orientation = 1;}
- else if(f == 1){
-   orientationServo.write(0);
-   orientation = 0;}
+                 
+//=====Recall=====\\ 
+void recall(){
+ status();
+ baseServo.write(initPos);
+ delay(100);
+ armServo.write(initPos);
+ delay(100);
+ rotateServo.write(initPos);
+ delay(100);
+ gripperServo.write(initPos);
+ delay(100);
  status();
 }
                  
-void automate();
-   sortServo.write(placeholderVal);
-   delay(placeholderTime);
-   sortServo.write(oppositePlaceholderVal);
-}
+//=====Status=====\\ 
+void status(){
+ if(digitalRead(statusLED) == LOW){
+  statusLED.write(HIGH);}
+ else{
+  statusLED.write(LOW);}
+} 
+                 
+//=====Initialize=====\\ 
+void initialize(){
+ baseServo.write(initPosB);
+ delay(100);
+ armServo.write(initPosA);
+ delay(100);
+ rotateServo.write(initPosR);
+ delay(100);
+ gripperServo.write(initPosG);
+ delay(100);
+ orientationServo.write(initPosO);
+ delay(100);
+ sortServo.write(InitPosS);
+ delay(100);
+ 
+ prevButtonB = digitalRead(buttonBlue);
+ prevButtonG = digitalRead(buttonGreen);
+ prevButtonR = digitalRead(buttonRed);
+ 
+ currentAngle = initPosG;
+ 
+ calibrate();
+ 
+ stabilize();
+} 
+                 
+//============================================Gyro Functions======================================================\\
+                 
+//=====Callibrate=====\\                                  
+void callibrate(){
+ status();
+ //
+ status();
+} 
+                
+//=====getGyroVals=====\\                 
+float getGyroVals(){
 
+}
+                 
+//=====Stabilize=====\\ 
+void stabilize(){
+ gyro = getGyroVals();
+ currentAngle -= gyro;
+ gripperServo.write(currentAngle);
+}
+                             
+//==========================================Servo Functions========================================================\\
+                 
+//=====Move=====\\                                              
+void move(Servo servoObj, int v, int currentAngle){
+ servoObj.write(currentAngle + v);
+ stabilize();
+ delay(movementSpeed);
+}
+                 
+//=====ServoMovement=====\\                  
 void servoMovement(){
  int rotateVal = analogRead(joyRX);
  int baseVal = analogRead(joyRY);
  int armVal = analogRead(joyLY);
+ 
+ int currentAngleR = rotateServo.read();
+ int currentAngleB = baseServo.read();
+ int currentAngleA = armServo.read();
  
  if(rotateVal > 800){
    move(rotateServo, velox);}
@@ -139,38 +262,3 @@ void servoMovement(){
    move(armServo, -velox);}
 }
                  
-void move(Servo servoObj, int v){
- servoObj.write(v);
- stabilize();
- delay(speed);
-}
-
-void recall(){
- status();
- baseServo.write(initPos);
- delay(100);
- armServo.write(initPos);
- delay(100);
- rotateServo.write(initPos);
- delay(100);
- gripperServo.write(initPos);
- delay(100);
- status();
-}
-                 
-void callibrate(){
- status();
- //Placeholder for code
- status();
-}
- 
-void status(){
- if(digitalRead(statusLED) == LOW){
-  statusLED.write(HIGH);}
- else{
-  statusLED.write(LOW);}
-} 
- 
- 
- 
- 
